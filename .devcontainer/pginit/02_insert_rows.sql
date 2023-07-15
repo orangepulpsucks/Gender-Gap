96e784e1b60c34737944f1b0c065ea1580d10541
orangepulpsucks <hahnya@plu.edu>
2023-07-15 00:20:48 +0000

    typo

--- .devcontainer/pginit/02_insert_rows.sql
+++
@@ -1,6 +0,0 @@
-copy theme(ApplicantNo, Age, EdLevel, Gender, MainBranch, YearsCode,
-           Country,HaveWorkedWith,ComputerSkills)
-from '/docker-entrypoint-initdb.d/seed_data/job_applicant_data.csv'
-delimiter ','
-csv header;
-
