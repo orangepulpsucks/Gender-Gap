6068a8f871c62e901b9f695eefc182f47490d7ca
orangepulpsucks <hahnya@plu.edu>
2023-07-15 05:01:12 +0000

    basic framework done

--- app.py
+++ app.py
@@ -12,41 +12,40 @@ app = Flask(__name__)
 # HOME -> 1
 @app.route('/')
 def render_sets1():
-    Age = request.args.get("Age", "")
-    EdLevel = request.args.get("Edlevel", "")
-    Gender = request.args.get("Gender", "")
-    MainBranch = request.args.get("MainBranch", "")
-    YearsCode = request.args.get("YearsCode", "")
-    Country = request.args.get("Country", "")
-    HaveWorkedWith = request.args.get("HaveWorkedWith", "")
-    ComputerSkills = request.args.get("ComputerSkills", "")
+    edlevel = request.args.get("edlevel", "")
+    gender = request.args.get("gender", "")
+    mainbranch = request.args.get("mainbranch", "")
+    yearscode = request.args.get("yearscode", "")
+    country = request.args.get("country", "")
+    haveworkedwith = request.args.get("haveworkedwith", "")
+    computerskills = request.args.get("computerskills", "")
     
-    sort_by = request.args.get("sort_by", "series_title")
+    sort_by = request.args.get("sort_by", "yearscode")
     sort_dir = request.args.get("sort_dir", "asc")
     limit = request.args.get("limit", 1000, type=int)
 
 #gender_d, edlevel_d, mainbranch_d, yearscode, computerskills,haveworkedwith, country
     from_where_clause1 = """
         from theme
-        where %(Gender)s is null or gender ilike %(Gender)s
-        and ( %(EdLevel)s is null or edlevel = %(EdLevel)s )
-        and ( %(MainBranch)s is null or mainbranch = %(MainBranch)s )
-        and ( %(YearsCode)s is null or yearscode ilike %(YearsCode)s )
-        and ( %(ComputerSkills)s is null or computerskills = %(ComputerSkills)s )
-        and ( %(HaveWorkedWith)s is null or haveworkedwith ilike %(HaveWorkedWith)s )
-        and ( %(Country)s is null or country ilike %(Country)s )
+        where %(gender)s is null or gender ilike %(gender)s
+        and ( %(edlevel)s is null or edlevel ilike %(edlevel)s )
+        and ( %(mainbranch)s is null or mainbranch ilike %(mainbranch)s )
+        and ( %(yearscode)s is null or yearscode = %(yearscode)s )
+        and ( %(computerskills)s is null or computerskills = %(computerskills)s )
+        and ( %(haveworkedwith)s is null or haveworkedwith ilike %(haveworkedwith)s )
+        and ( %(country)s is null or country ilike %(country)s )
     """
 
     params1 = {
-        "Gender": f"%{Gender}%",
-        "EdLevel": f"%{EdLevel}%",
-        "MainBranch": f"%{MainBranch}",
-        "YearsCode": int(YearsCode) if YearsCode and YearsCode.isdigit() else 0
-        if YearsCode and not YearsCode.isdigit() else None,
-        "ComputerSkills": int(ComputerSkills) if ComputerSkills and ComputerSkills.isdigit() else 0
-        if ComputerSkills and not ComputerSkills.isdigit() else None,
-        "HaveWorkedWith": f"%{HaveWorkedWith}",
-        "Country": f"%{Country}",
+        "gender": f"%{gender}%",
+        "edlevel": f"%{edlevel}%",
+        "mainbranch": f"%{mainbranch}",
+        "yearscode": int(yearscode) if yearscode and yearscode.isdigit() else 0
+        if yearscode and not yearscode.isdigit() else None,
+        "computerskills": int(computerskills) if computerskills and computerskills.isdigit() else 0
+        if computerskills and not computerskills.isdigit() else None,
+        "haveworkedwith": f"%{haveworkedwith}",
+        "country": f"%{country}",
 
         "sort_by": sort_by,
         "sort_dir" : sort_dir,
@@ -58,23 +57,21 @@ def render_sets1():
             return "desc" if sort_dir == "asc" else "asc"
         else:
             return "asc"
-   #gender_d, edlevel_d, mainbranch_d, yearscode, computerskills,haveworkedwith, country
-     
+             
     with conn.cursor() as cur:
-        cur.execute(f"""gender, edlevel, mainbranch, yearscode, computerskills, haveworkedwith, country
+        cur.execute(f"""select gender, edlevel, mainbranch, yearscode, computerskills, haveworkedwith, country
                         {from_where_clause1} 
                         order by {sort_by} {sort_dir} 
                         limit %(limit)s 
                     """,
                     params1)
         results1 = list(cur.fetchall())  
+        
+        cur.execute(f"select count(*) as count {from_where_clause1}", params1)
+        count = cur.fetchone()["count"]
 
 
     return render_template("home.html",
-                           params1=request.args
+                           params1=request.args,
+                           result_count =count
                            )    
-
-@app.route("/about")
-def render_sets2():
-    return render_template("about.html")
-   
