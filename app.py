0bc280f8a140939500b02b0bbda94760cdc0c784
orangepulpsucks <hahnya@plu.edu>
2023-07-16 07:15:02 +0000

    final commit for this hackathon

--- app.py
+++ app.py
@@ -79,73 +79,24 @@ def render_sets1():
                            get_sort_dir = get_sort_dir
                            ) 
     
-# VISUALIZATIONS -> 2
-@app.route('/Visualizations')
-def render_sets2():
-    edlevel = request.args.get("edlevel", "%")
-    gender = request.args.get("gender", "%")
-    mainbranch = request.args.get("mainbranch", "%")
-    yearscode = request.args.get("yearscode", "")
-    country = request.args.get("country", "")
-    haveworkedwith = request.args.get("haveworkedwith", "")
-    computerskills = request.args.get("computerskills", "")
-    sort_by = request.args.get("sort_by", "yearscode")
-    sort_dir = request.args.get("sort_dir", "asc")
-    limit = request.args.get("limit", 500, type=int)
-
-#note to self: ilike is case insensitive!
-    from_where_clause2 = """
-        from theme
-        where %(gender)s is null or gender like %(gender)s
-        and (  %(edlevel)s is null or edlevel like %(edlevel)s )
-        and ( %(mainbranch)s is null or mainbranch like %(mainbranch)s)
-        and ( %(yearscode)s is null or yearscode = %(yearscode)s )
-        and ( %(computerskills)s is null or computerskills = %(computerskills)s )
-        and ( %(haveworkedwith)s is null or haveworkedwith ilike %(haveworkedwith)s )
-        and ( %(country)s is null or country ilike %(country)s )
-    """
-
-    params2 = {
-        "gender": f"{gender}",
-        "edlevel": f"{edlevel}",
-        # "mainbranch": f"{mainbranch}" if mainbranch != '' else '%',
-        "mainbranch": f"{mainbranch}",
-        "yearscode": int(yearscode) if yearscode and yearscode.isdigit() else 0
-        if yearscode and not yearscode.isdigit() else None,
-        "computerskills": int(computerskills) if computerskills and computerskills.isdigit() else 0
-        if computerskills and not computerskills.isdigit() else None,
-        "haveworkedwith": f"%{haveworkedwith}",
-        "country": f"%{country}",
-
-        "sort_by": sort_by,
-        "sort_dir" : sort_dir,
-        "limit" : limit
-}
-
-    def get_sort_dir(col):
-        if col== sort_by:
-            return "desc" if sort_dir == "asc" else "asc"
-        else:
-            return "asc"
-             
-    with conn.cursor() as cur:
-        cur.execute(f"""select gender, edlevel, mainbranch, yearscode, computerskills, haveworkedwith, country
-                        {from_where_clause2} 
-                        order by {sort_by} {sort_dir} 
-                        limit %(limit)s 
-                    """,
-                    params2)
-        results2 = list(cur.fetchall())  
-        
-        cur.execute(f"select count(*) as count {from_where_clause2}", params2)
-        count = cur.fetchone()["count"]
-
-  
-    
-    return render_template("visualizations.html",
-                           theme=results2,
-                           params2=request.args,
-                           result_count=count,
-                           get_sort_dir = get_sort_dir
+# VISUALIZATIONS-BY-GENDER 
+@app.route('/Visualizations-by-Gender')
+def visualizations_by_gender():
+    return render_template("visualizations-by-gender.html"
                            ) 
+    
+# VISUALIZATIONS-BY-EDUCATION 
+@app.route('/Visualizations-by-Education')
+def visualizations_by_education():
+    return render_template("visualizations-by-education.html")
+
+# VISUALIZATIONS-BY-BRANCH 
+@app.route('/Visualizations-by-Branch')
+def visualizations_by_branch():
+    return render_template("visualizations-by-branch.html")
+
+# ABOUT
+@app.route('/About')
+def about():
+    return render_template("about.html")
 
